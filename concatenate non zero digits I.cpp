134b9e8b76b047df9b6de74e0f386effbcfa9a9e
sarthakchanda98 <sarthakchanda98@gmail.com>
2026-07-07 19:45:30 +0530

    Implement sum and multiply of non-zero digits

---
+++ concatenate non zero digits I.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long sumAndMultiply(int n) 
+    {
+        long long x=0;
+        long long sum = 0;
+        long long pow10 = 1;
+        while(n!=0)
+        {
+            int d = n%10;
+            sum += d;
+            if(d>0)
+            {
+            x += d*pow10;
+            pow10*=10;
+            }
+            n /= 10;
+        }
+        return x*sum;
+    }
+};
