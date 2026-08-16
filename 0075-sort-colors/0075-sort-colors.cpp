36e916dab74eadd9ab9fc508ead4e0d7783629ee
sarthakchanda98 <sarthakchanda98@gmail.com>
2026-08-16 16:58:15 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.08%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    void sortColors(vector<int>& A) 
+    void sortColors(vector<int>& nums) 
     {
-        //dutch national flag algo
-        int low = 0;
-        int mid = 0;
-        int high = A.size()-1;
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
         int temp;
         while(mid<=high)
         {
-            if(A[mid]==0)
+            if(nums[mid]==0)
             {
-                temp = A[mid];
-                A[mid] = A[low];
-                A[low] = temp;
+                temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
                 low++;
                 mid++;
             }
-            else if(A[mid] == 1)
+            else if(nums[mid]==1)
             {
                 mid++;
             }
-            else if(A[mid]==2)
+            else if(nums[mid]==2)
             {
-                temp = A[mid];
-                A[mid] = A[high];
-                A[high] = temp;
+                temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
                 high--;
             }
         }
